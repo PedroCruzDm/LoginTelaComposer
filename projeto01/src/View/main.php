05b90f4c35f367057ca2321246fc21eaf61590b6
O Famosoo Pedro <108557012+PedroCruzDm@users.noreply.github.com>
2024-08-29 22:31:47 -0300

    commitado tela login
    apenas up 01

---
+++ projeto01/src/View/main.php
@@ -0,0 +1,17 @@
+<html>
+    <body>
+        <center>
+            <form class="form-login" id="login" method="_POST">
+              <h1>Login</h1>
+         <br>
+          <label>E-mail:<label>
+         <br>
+            <input type="e-mail" name="e-mail" id="e-mail" placeholder="Digite o seu E-mail"><br>
+          <label>Senha:</label> <br>
+            <input type="password" name="senha" id="senha" placeholder="Digite sua senha"> <br>
+            <!-- BTN enviar --><br><hr>
+        <button type="submit" value="Enviar">Enviar</button>
+        </form>
+    </center>
+ </body>
+<html>
---
+++ projeto01/src/index.php
@@ -0,0 +1,5 @@
+<?php 
+include('./vendor/autoload.php');
+include('./view/main.php');
+//$email = 
+?>
